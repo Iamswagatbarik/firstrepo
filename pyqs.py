8a659619977a60415911543ca8e032c68ba8a05f
Swagat kumar Barik <skbarik2300@gmail.com>
2023-04-24 10:30:12 +0530

    test

---
+++ pyqs.py
@@ -0,0 +1,3 @@
+reg="2041002022"
+year="2020"
+print ('My registation number is {} and I have taken admission  {}' .format(reg,year))
---
+++ pytuto1.py
@@ -0,0 +1,16 @@
+#print ("Hello world")
+#print(1+2)
+#print (10>5)
+#a = 10
+#b = 11
+#print (a)
+#a=12
+#print (a)
+#print (a,b)
+#print (id(a),id(b))
+# a="Hello"
+# b="Bapa"
+# print (a+" "+b)
+# c=30
+# print(c+30)
+
