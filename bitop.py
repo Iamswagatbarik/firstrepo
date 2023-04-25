23faf314b1b82cdf26d8860041bc1536a0c003fa
Swagat kumar Barik <skbarik2300@gmail.com>
2023-04-25 20:22:03 +0530

    ain

---
+++ bitop.py
@@ -0,0 +1,20 @@
+x=10
+y=12
+#and'&'
+print (bin(x))
+print (bin(y))
+print (bin(x&y))
+print (x&y)
+print (x&y, bin(x&y))
+#or'|'
+print (bin(x))
+print (bin(y))
+print (bin(x|y))
+print (x|y)
+print (x|y, bin(x|y))
+#xor'^'
+print (bin(x))
+print (bin(y))
+print (bin(x^y))
+print (x^y)
+print (x^y, bin(x^y))
