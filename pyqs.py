cf297ac62eeb7b90ea295e3c634dcaa5e06b4d9b
Swagat kumar Barik <skbarik2300@gmail.com>
2023-04-26 10:35:54 +0530

    main

--- pyqs.py
+++ pyqs.py
@@ -1,3 +1,3 @@
 reg="2041002022"
 year="2020"
-print ('My registation number is {} and I have taken admission  {}' .format(reg,year))
+print ('My registation number is {} and I have taken admission in the year  {}' .format(reg,year))
